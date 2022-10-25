11bf45711934c325a038ac2cdbfc41dc8ea2def5
suji5262 <suji5262@gmail.com>
2022-10-25 21:43:29 +0900

    22.10.25 upload

---
+++ javaBasicEX/collections_framework/HashSet/hashSet1.java
@@ -0,0 +1,26 @@
+package collections_framework.HashSet;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class hashSet1 {
+    public static void main(String[] args) {
+        Object[] objArr = {"1", new Integer(1), "2", "2", "3", "3", "4", "4", "4"};
+        Set set = new HashSet();
+
+        for (int i = 0; i < objArr.length; i++) {
+//            set.add(objArr[i]);
+            System.out.println(objArr[i] + "=" + set.add(objArr[i]));
+
+        }
+
+        System.out.println(set);
+
+        Iterator it = set.iterator();
+
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+}
